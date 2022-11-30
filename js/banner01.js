8ae6cb40be562e874a3a404cdb2c30fe9b058eb8
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2022-11-30 17:44:48 +0900

    Add files via upload

--- js/banner01.js
+++ js/banner01.js
@@ -2,22 +2,27 @@ $(function () {
 
     $('.topBanner i').on('click', function () {
         $('.topBanner').toggleClass('on');
-    })
+    });
 
     $('.topBanner2 i').on('click', function () {
         $('.topBanner2 .inner').slideToggle(600);
-    })
+    });
+
+    $('.leftBanner i').on('click', function () {
+        $('.leftBanner').toggleClass('on');
+    });
+
+    $('.rightBanner i').on('click', function () {
+        $('.rightBanner').toggleClass('on');
+    });
+
     $('.bottomBanner i').on('click', function () {
         $('.bottomBanner .inner').slideToggle();
-    })
-    $('.bottomBanner i').on('click', function () {
-        $('.bottomBanner .inner').slideToggle();
-    })
+    });
 
     $('.popUp button').on('click', function () {
         $(this).parent().hide();
-    })
-
+    });
 
 })
 
